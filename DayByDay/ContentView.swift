0d9d27483061114afbc8cde2719f655a6044d36b
Jerome Campbell <jmasashicampbell@gmail.com>
2020-04-30 14:26:25 -0700

    Initial Commit

---
+++ DayByDay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DayByDay
+//
+//  Created by Jerome Campbell on 4/30/20.
+//  Copyright © 2020 Jerome Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
